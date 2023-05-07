ef24d0168b84bd36f61eba62e6b9a7ab81926027
Hanaasagi <ambiguous404@gmail.com>
2023-05-08 00:15:24 +0900

    align right

--- src/lib/rewriteReadmeToIncludeCardText.ts
+++ src/lib/rewriteReadmeToIncludeCardText.ts
@@ -1,5 +1,5 @@
-import {MARK} from '../constant'
-import {Language, Score, Theme} from '../types/types'
+import { MARK } from '../constant'
+import { Language, Score, Theme } from '../types/types'
 
 const createCardText = ({
   shareId,
@@ -16,17 +16,16 @@ const createCardText = ({
   cardWidth: string
   showUpdateTime: boolean
 }): string => {
-  const imageUrl = `https://lapras-card-generator.vercel.app/api/svg?e=${
-    score.eScore
-  }&b=${score.bScore}&i=${score.iScore}&b1=${encodeURIComponent(
-    theme.background.first
-  )}&b2=${encodeURIComponent(theme.background.second)}&i1=${encodeURIComponent(
-    theme.icon.first
-  )}&i2=${encodeURIComponent(theme.icon.second)}&l=${lang}`
+  const imageUrl = `https://lapras-card-generator.vercel.app/api/svg?e=${score.eScore
+    }&b=${score.bScore}&i=${score.iScore}&b1=${encodeURIComponent(
+      theme.background.first
+    )}&b2=${encodeURIComponent(theme.background.second)}&i1=${encodeURIComponent(
+      theme.icon.first
+    )}&i2=${encodeURIComponent(theme.icon.second)}&l=${lang}`
 
   const updateTime = showUpdateTime ? `  \nLast Updated on ${new Date().toLocaleString()}` : ''
 
-  return `<a href="https://lapras.com/public/${shareId}" target="_blank" rel="noopener noreferrer"><img src="${imageUrl}" width="${cardWidth}" ></a>${updateTime}`
+  return `<a align="right" href="https://lapras.com/public/${shareId}" target="_blank" rel="noopener noreferrer"><img src="${imageUrl}" width="${cardWidth}" ></a>${updateTime}`
 }
 
 export const rewriteReadmeToIncludeCardText = (
@@ -52,6 +51,6 @@ export const rewriteReadmeToIncludeCardText = (
     throw new Error(`Error: README.mdにカードを挿入するためのMARKER文字列が見つかりませんでした。"${MARK.START + MARK.END}" をREADME.mdに追加してください`)
   }
 
-  const cardText = createCardText({shareId, score, theme, lang, cardWidth, showUpdateTime})
+  const cardText = createCardText({ shareId, score, theme, lang, cardWidth, showUpdateTime })
   return readme.replace(markerPattern, `$1\n${cardText}\n$2`)
 }
